913120fbad8b146f6736c0e0cb2b62c9697b699c
reinforest915820 <alexandre915820@gmail.com>
2022-11-02 15:05:46 -0400

    authActions.js / signup completed

--- src/store/actions/authActions.js
+++ src/store/actions/authActions.js
@@ -1,1 +1,24 @@
+import axios from "axios";
+import { url } from "../../api";
+import { toast } from "react-toastify";
 
+export const signUp = (user) => {
+  return (dispatch) => {
+    axios
+      .post(`${url}/signup`, user)
+      .then((token) => {
+        localStorage.setItem("token", token.data);
+
+        dispatch({
+          type: "SIGN_UP",
+          token: token.data,
+        });
+      })
+      .catch((error) => {
+        console.log(error.response);
+        toast.error(error.response?.data, {
+          position: toast.POSITION.BOTTOM_RIGHT,
+        });
+      });
+  };
+};
